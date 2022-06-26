775ae2fe14e64a6a20edf74ec9709f310383b271
Keks <keks@htmlacademy.ru>
2022-06-26 21:19:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import './data';
+import './util';
+import './picture';
+import {createPosts} from './data';
+import {createPhoto} from './picture';
+
+
+const photo = createPosts(1);
+createPhoto(photo);
